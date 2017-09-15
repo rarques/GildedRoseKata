8d2ddcdde71c771383cb2dfeef60268e3a24d494
Roger Arques <rarquesvall@gmail.com>
2017-09-16 00:12:06 +0200

    Created baseline classes and tests to help refactoring

---
+++ src/main/java/com/gildedrose/ItemBaseLine.java
@@ -0,0 +1,21 @@
+package com.gildedrose;
+
+public class ItemBaseLine {
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public ItemBaseLine(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+   @Override
+   public String toString() {
+        return this.name + ", " + this.sellIn + ", " + this.quality;
+    }
+}
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -11,7 +12,42 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
+    }
+
+    @Test
+    public void testGildedRoseRefactoredAndGildedRoseBaseLineDoTheSamThing() {
+        Item[] baseLineItems = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
+
+        Item[] items = new Item[]{
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)};
+
+        GildedRoseBaseLine app = new GildedRoseBaseLine(baseLineItems);
+        GildedRose refactoredApp = new GildedRose(items);
+
+        app.updateQuality();
+        refactoredApp.updateQuality();
+
+        for (int itemIndex = 0; itemIndex < items.length; itemIndex++) {
+            assertThat(refactoredApp.items[itemIndex].name, is(app.items[itemIndex].name));
+            assertThat(refactoredApp.items[itemIndex].quality, is(app.items[itemIndex].quality));
+            assertThat(refactoredApp.items[itemIndex].sellIn, is(app.items[itemIndex].sellIn));
+        }
     }
 
 }

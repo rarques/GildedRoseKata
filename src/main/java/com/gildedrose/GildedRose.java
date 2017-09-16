d432c5b3b0d42b20384aac69144ebe45753b2020
Roger Arques <rarquesvall@gmail.com>
2017-09-16 15:51:13 +0200

    Extracted updateQuality method to a new class

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,9 +1,9 @@
 package com.gildedrose;
 
 public class GildedRose {
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
+
+    private final QualityUpdater qualityUpdater = new QualityUpdater();
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -12,54 +12,11 @@ public class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-
-            /* Update sellIn for all items except legendary items */
-            if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS))
-                ;
-            else
-                item.sellIn--;
-
-
-            if (item.name.equals(AGED_BRIE)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
-            } else if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-
-            } else {
-                item.quality = item.quality - 1; // Behaviour for a normal item
-            }
-
-
-            /* Update quality of the items that update quality depending on the sellIn property */
-            if (item.sellIn < 0) {
-                if (item.name.equals(AGED_BRIE)) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                } else if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT)) {
-                    item.quality = 0;
-                } else if (item.name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-                    ;
-                }
-            }
-
-
+            qualityUpdater.updateItemQuality(item);
         }
+    }
+
+    private void updateItemQuality(Item item) {
+        qualityUpdater.updateItemQuality(item);
     }
 }
